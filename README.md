62419aa20e63eb5861c293c5533370f839bca376
Chris Alberti <christopherjalberti@gmail.com>
2018-05-12 11:27:28 -0400

    update README

--- README.md
+++ README.md
@@ -2,4 +2,15 @@ Tic Tac Toe Game for Terminal
 
 # Description
 
-Player 1 can choose their symbol ('X' or 'O').
+This Tic Tac Toe game has 3 gameplay options:
+1 player (human player vs computer player)
+2 players (human player vs human player)
+0 players (computer player vs computer player) https://www.youtube.com/watch?v=F7qOV8xonfY
+
+1 player mode has 3 difficulty options:
+easy: computer player will select random available positions.
+medium: computer player will block the first "threat" it detects (if human player has 2 in a row somewhere on the board).
+If no threat is detected, computer player will select a random available position.
+hard: computer player will first check to see if it has 2 in a row somewhere on the board. If a winning move
+is possible, it will make that move. If there is no winning move, the computer player will block any threats, if detected.
+If no winning moves or threats are detected, the computer player will select a random available position.
--- computer_player.rb
+++ computer_player.rb
@@ -1,12 +1,17 @@
 class ComputerPlayer
 
-  attr_reader :name, :symbol, :difficulty, :delay
+  attr_reader :name, :symbol, :opposing_symbol, :difficulty, :delay
 
   def initialize(name, symbol, difficulty, delay)
     @name = name
     @symbol = symbol
     @difficulty = difficulty
     @delay = delay
+    if symbol == "X"
+      @opposing_symbol = "O"
+    else
+      @opposing_symbol = "X"
+    end
   end
 
   def get_move(board)
@@ -27,41 +32,27 @@ class ComputerPlayer
     moves = []
     (0..8).each {|pos| moves << pos if board.grid[pos] != "X" && board.grid[pos] != "O"}
 
-    if symbol == "X"
-      opposing_symbol = "O"
-    else
-      opposing_symbol = "X"
-    end
-
-    moves.each do |move|
-      board.grid[move] = opposing_symbol
-      if board.is_winning_move?(opposing_symbol)
-        board.grid[move] = move + 1
-        return move
-      else
-        board.grid[move] = move + 1
-      end
-    end
+    threat_move = check_moves(board, moves, opposing_symbol)
+    return threat_move if threat_move
     moves.sample
   end
 
   def hard_move(board)
-    if delay
-      sleep(3)
-    end
+    sleep(3) if delay
+
     moves = []
     (0..8).each {|pos| moves << pos if board.grid[pos] != "X" && board.grid[pos] != "O"}
 
-    if symbol == "X"
-      opposing_symbol = "O"
-    else
-      opposing_symbol = "X"
-    end
+    return 4 if moves.include?(4)
 
-    if moves.include?(4)
-      return 4
-    end
+    winning_move = check_moves(board, moves, symbol)
+    return winning_move if winning_move
+    threat_move = check_moves(board, moves, opposing_symbol)
+    return threat_move if threat_move
+    moves.sample
+  end
 
+  def check_moves(board, moves, symbol)
     moves.each do |move|
       board.grid[move] = symbol
       if board.is_winning_move?(symbol)
@@ -71,16 +62,6 @@ class ComputerPlayer
         board.grid[move] = move + 1
       end
     end
-
-    moves.each do |move|
-      board.grid[move] = opposing_symbol
-      if board.is_winning_move?(opposing_symbol)
-        board.grid[move] = move + 1
-        return move
-      else
-        board.grid[move] = move + 1
-      end
-    end
-    moves.sample
+    false
   end
 end
